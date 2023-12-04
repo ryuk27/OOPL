73c2f00e3d415fd61326c95a3bdaf7b3544df14d
Ram <107495081+ryuk27@users.noreply.github.com>
2023-12-05 00:54:52 +0530

    Assignment 2
    Identify commonalities and differences between Publication, Book and Magazine classes. Title, Price, Copies are common instance variables and saleCopy is common method. The differences are, Bookclass has author and orderCopies(). Magazine Class has methods orderQty, Current issue, receiveissue().Write a program to find how many copies of the given books are ordered and display total sale of publication.

---
+++ SalesProgram.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+
+class Publication {
+    private String title;
+    private double price;
+    protected int copies;
+
+    public Publication(String title, double price, int copies) {
+        this.title = title;
+        this.price = price;
+        this.copies = copies;
+    }
+
+    public double saleCopy(int quantity) {
+        if (quantity <= copies) {
+            copies -= quantity;
+            return getPrice() * quantity;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+class Book extends Publication {
+    private String author;
+
+    public Book(String title, double price, int copies, String author) {
+        super(title, price, copies);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public double saleCopy(int quantity) {
+        if (quantity <= getCopies()) {
+            int availableCopies = getCopies();
+            availableCopies -= quantity;
+            setCopies(availableCopies);
+            return getPrice() * quantity * 0.9; // 10% discount for books
+        } else {
+            return 0.0;
+        }
+    }
+}
+
+class Magazine extends Publication {
+    private int currentIssue;
+
+    public Magazine(String title, double price, int copies, int currentIssue) {
+        super(title, price, copies);
+        this.currentIssue = currentIssue;
+    }
+
+    public int getCurrentIssue() {
+        return currentIssue;
+    }
+
+    @Override
+    public double saleCopy(int quantity) {
+        if (quantity <= getCopies()) {
+            int availableCopies = getCopies();
+            availableCopies -= quantity;
+            setCopies(availableCopies);
+            return getPrice() * quantity; // No discount for magazines
+        } else {
+            return 0.0;
+        }
+    }
+}
+
+public class SalesProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create Book and Magazine instances
+        Book book = new Book("Java Programming", 29.99, 100, "John Doe");
+        Magazine magazine = new Magazine("Tech Magazine", 5.99, 200, 10);
+
+        // Selling copies using overridden methods
+        System.out.println("Book Details:");
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Price: Rs. " + book.getPrice());
+        System.out.println("Available Copies: " + book.getCopies());
+
+        System.out.print("Enter the quantity of books to order: ");
+        int bookQty = scanner.nextInt();
+        double bookSales = book.saleCopy(bookQty);
+        System.out.println("Total Book Sales: Rs. " + bookSales);
+
+        // Similar code for selling magazines using the overridden method
+        System.out.println("\nMagazine Details:");
+        System.out.println("Title: " + magazine.getTitle());
+        System.out.println("Current Issue: " + magazine.getCurrentIssue());
+        System.out.println("Price: Rs. " + magazine.getPrice());
+        System.out.println("Available Copies: " + magazine.getCopies());
+
+        System.out.print("Enter the quantity of magazines to order: ");
+        int magazineQty = scanner.nextInt();
+        double magazineSales = magazine.saleCopy(magazineQty);
+        System.out.println("Total Magazine Sales: Rs. " + magazineSales);
+
+        // Calculate and display total publication sales
+        double totalSales = bookSales + magazineSales;
+        System.out.println("\nTotal Publication Sales: Rs. " + totalSales);
+
+        scanner.close(); // Close the scanner at the end
+    }
+}
